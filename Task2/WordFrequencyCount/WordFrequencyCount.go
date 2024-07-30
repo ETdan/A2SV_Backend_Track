eaba9ec13bb508c08ed8c7027afc581d9107eb2a
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-07-31 01:47:33 +0300

    alkdj

--- Task2/WordFrequencyCount/WordFrequencyCount.go
+++ Task2/WordFrequencyCount/WordFrequencyCount.go
@@ -4,24 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
+
+func stripRegex(in string) string {
+	reg, _ := regexp.Compile("[^a-zA-Z0-9 ]+")
+	return reg.ReplaceAllString(in, "")
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your Word: ")
 	word, _ := reader.ReadString('\n')
-	// fmt.Println(word)
-	// fmt.Printf("%T %v", word, word)
-	// arr := []string{}
-	arr := strings.Split(word, " ")
+	arr := strings.Split(strings.ToLower(word), " ")
+	for index, val := range arr {
+
+		arr[index] = stripRegex(val)
+
+	}
+
 	counter := make(map[string]int)
 	for _, val := range arr {
 		counter[val] += 1
 	}
 	fmt.Println("")
 	for key, val := range counter {
-
 		fmt.Printf("%-35v %v\n", key, val)
 	}
 }
